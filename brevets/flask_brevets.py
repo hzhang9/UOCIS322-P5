b24deab5723f954bb2b10c3186b3bcf48ddb2a62
Haoran Zhang <hzhang9@testium.cs.uoregon.edu>
2021-05-23 16:16:59 -0700

    Now can deal with error or empty input

--- brevets/flask_brevets.py
+++ brevets/flask_brevets.py
@@ -84,12 +84,51 @@ def submit():
     km=request.form.getlist("km")
     miles=request.form.getlist("miles")
     location=request.form.getlist('location')
-    for i in range(len(open_time)):
-        if(open_time[i]!=""):
-            if (location[i]==""):
+    brevet_dist=request.args.get('brevet_dist',type=int)
+    counter=0
+    repeat=False
+    repeat_check=[]
+    in_order=True
+    temp_km=km[0]
+    space_check=False
+    have_empty=False
+    for i in range(len(km)):
+        if(km[i]!=""):
+            if location[i]=="":
                 location[i]="None"
+            if space_check==True:
+                have_empty=True
+            if km[i] in repeat_check:
+                repeat=True
+            if km[i]<temp_km:
+                in_order=False
+            #if float(km[i])>float(1.2*brevet_dist):
+                #message="Input distance cannot over brevet distance more than 20%"
+                #return flask.render_template('error_submit.html',message=message)
+            #if float(km[i])<float(brevet_dist):
+                #message="Last input control distance must over brevet distance"
+                #return flask.render_template('error_submit.html',message=message)
+
+            temp_km=km[i]
+            app.logger.debug(temp_km)
+            repeat_check.append(km[i]) 
             db.tododb.insert_one({'open':open_time[i],'close':close_time[i],'km':km[i],'miles':miles[i],'location':location[i]}) 
-    if 
+            counter+=1
+        else:
+            space_check=True
+
+    if counter==0:
+        message="Cannot submit empty"
+        return flask.render_template('error_submit.html',message=message)
+    elif repeat==True:
+        message="Cannot submit repeat control distances"
+        return flask.render_template('error_submit.html',message=message)
+    elif in_order==False:
+        message="Should input control distances from small to large"
+        return flask.render_template('error_submit.html',message=message)
+    elif have_empty==True:
+        message="Don't left empty between two valid  control time"
+        return flask.render_template('error_submit.html',message=message)
     return redirect(url_for('index'))
 
 #############
